a4c58dd36874bfbd26665fa471727bde5a38f0b6
aziliz35 <azilizba@gmail.com>
2016-02-23 18:20:01 +0100

    Partie 1: DONE

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,4 +1,15 @@
-var pokeApp = angular.module('pokedex', ['ngResource']);
+var pokeApp = angular.module('pokedex', ['ngResource'])
+        .controller('pokcontroller', ['$scope', '$log', function($scope, $log) {
+            $scope.test = [
+                {name:'pikachu',id:'1'},
+                {name:'charizard',id:'2'},
+                {name:'snorlax',id:'3'},
+                {name:'bulbasaur',id:'4'}
+            ];
+        $scope.idpok='';
+            $scope.$log = $log;
+        }])
+    ;
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
