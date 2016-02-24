8d42a74daad39011b2d34bb729c4959531cf504c
aziliz35 <azilizba@gmail.com>
2016-02-24 17:40:26 +0100

    Q11

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,6 +1,10 @@
+
+
+
+
 var pokeApp = angular.module('pokedex', ['ngResource'])
-        .controller('pokcontroller', ['$scope', '$log', function($scope, $log) {
-            $scope.test = [
+        .controller('pokcontroller', ['$scope', '$log','$http', function($scope, $log,$http) {
+            $scope.listepokemons = [
                 {name:'pikachu',id:'1'},
                 {name:'charizard',id:'2'},
                 {name:'snorlax',id:'3'},
@@ -8,11 +12,19 @@ var pokeApp = angular.module('pokedex', ['ngResource'])
             ];
         $scope.idpok='';
             $scope.$log = $log;
+
+            $http.get("http://pokeapi.co/api/v2/pokemon/")
+                .then(function(response) {
+                    $scope.listep = response.data;
+                });
         }])
+
     ;
+
+
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
-}]);
 
+}]);
 var pokeApiUrl = "http://pokeapi.co/"
